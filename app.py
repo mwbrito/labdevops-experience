84b448d341096b17be32c181ff952b80c9b2073a
Marcos Wilson Sarinho Brito <mwbrito@gmail.com>
2023-03-16 23:19:48 -0300

    apagando parte codigo

--- app.py
+++ app.py
@@ -8,6 +8,3 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "<h1>Hello World with Github Actions/GCP</h1><br><br><b>#fiap #10aso</b>"
-
-if __name__ == '__main__':
-    app.run()
