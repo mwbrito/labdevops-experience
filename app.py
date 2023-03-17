003ff76dc6a956b999a9e680677d34a4704e5c83
Marcos Wilson Sarinho Brito <mwbrito@gmail.com>
2023-03-16 22:03:28 -0300

    integrando SonarCloud

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "<h1>Hello World with Github Actions/Heroku</h1><br><br><b>#fiap #10aso</b>"
+    return "<h1>Hello World with Github Actions/GCP</h1><br><br><b>#fiap #10aso</b>"
 
 if __name__ == '__main__':
     app.run()
